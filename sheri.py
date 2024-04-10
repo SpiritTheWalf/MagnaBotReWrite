b68c5d1eb4fe439ab2f1f6e8c425ae91df9ac364
SpiritTheWalf <161088035+SpiritTheWalf@users.noreply.github.com>
2024-04-10 21:49:48 +0100

    Added sheri.py

---
+++ sheri.py
@@ -0,0 +1,116 @@
+import os
+import discord
+import requests
+
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord import app_commands
+
+load_dotenv()
+API_KEY = os.getenv("SHERI_API_KEY")
+
+
+class SheriCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.api_url = "https://sheri.bot/api"
+        self.api_key = API_KEY
+
+    @commands.command(name="endpoints")
+    async def endpoints(self, ctx):
+        if ctx.channel.is_nsfw():
+            await ctx.send("# SFW ENDPOINTS\n\nbday, belly_rub, blep, bonk, boop, bunny, cat, cry, cuddle, deer, fox, "
+                           "goat, hold, horse, hug, husky, kiss, lick, lion, mur, nature, pat, paws, pig, plane, "
+                           "pokemon, proposal, sfwsergal, shiba, snek, snep, tiger, turkey, turtle, wolves, "
+                           "yeen\n\n\n\n# NSFW ENDPOINTS\n\n69, anal, bang, bisexual, boob, boobwank, booty, "
+                           "christmas, cumflation, dick, dick_wank, dickmilk, dickorgy, dp, fbound, fcreampie, "
+                           "femboypresentation, finger, fpresentation, frot, fseduce, fsolo, ftease, futabang, gay, "
+                           "gay_bang, gif, handjob, herm_bang, impregnated, jockstraps, lesbian, lesbian_bang, lick, "
+                           "maws, mbound, mcreampie, mpresentation, mseduce, msolo, mtease, nboop, nbrony, nbulge, "
+                           "ncomics, ncuddle, ndeer, nfelkins, nfemboy, nfox, nfuta, ngroup, nhold, nhug, nhusky, "
+                           "nkiss, nleopard, nlick, npanther, npat, npokemon, nprotogen, nscalies, nsfwselfies, "
+                           "nsolo, nspank, ntease, ntrap, pawjob, pawlick, pegging_bang, petplay, pregnant, "
+                           "pussy, pussy_eating, ride, rimjob selfsuck, sfwsergal, straight_bang, suck, "
+                           "tentacles, toys, videos, vore, vorefanal, voreforal, vorefunbirth, voremanal, voremcock, "
+                           "voremoral, yiff")
+        else:
+            await ctx.send("SFW ENDPOINTS\n\nbday, belly_rub, blep, bonk, boop, bunny, cat, cry, cuddle, deer, fox, "
+                           "goat, hold, horse, hug, husky, kiss, lick, lion, mur, nature, pat, paws, pig, plane, "
+                           "pokemon, proposal, rpanda, sfwsergal, shiba, snek, snep, tiger, turkey, turtle, wolves, "
+                           "yeen")
+
+    @app_commands.command(name="media", description="Pulls an image from a specified image")
+    async def media(self, inter: discord.Interaction, endpoint: str = None):
+        try:
+            sfw_endpoints = ["bday", "belly_rub", "blep", "bonk", "boop", "bunny", "cat", "cry", "cuddle", "deer",
+                             "fox",
+                             "goat", "hold", "horse", "hug", "husky", "kiss", "lick", "lion", "mur", "nature",
+                             "pat", "paws",
+                             "pig", "plane", "pokemon", "proposal", "rpanda", "sfwsergal", "shiba", "snek",
+                             "snep", "tiger",
+                             "turkey", "turtle", "wolves", "yeen"]
+            nsfw_endpoints = ["69", "anal", "bang", "bisexual", "boob",
+                              "boobwank",
+                              "booty", "christmas", "cumflation", "cuntboy", "cuntboy_bang", "dick",
+                              "dick_wank",
+                              "dickmilk", "dickorgy", "dp", "fbound", "fcreampie", "femboypresentation", "finger",
+                              "fpresentation",
+                              "frot", "fseduce", "fsolo", "ftease", "futabang", "gay", "gay_bang", "gif",
+                              "handjob", "herm_bang",
+                              "impregnated", "jockstraps", "lesbian", "lesbian_bang", "maws", "mbound",
+                              "mcreampie", "mpresentation",
+                              "mseduce", "msolo", "mtease", "nboop", "nbrony", "nbulge", "ncomics", "ncuddle",
+                              "ndeer", "nfelkins",
+                              "nfemboy", "nfox", "nfuta", "ngroup", "nhold", "nhug", "nhusky", "nkiss",
+                              "nleopard", "nlick", "npanther",
+                              "npat", "npokemon", "nprotogen", "nscalies", "nsfwselfies", "nsolo", "nspank",
+                              "ntease", "ntrap", "pawjob",
+                              "pawlick", "paws", "pegging_bang", "petplay", "pregnant", "pussy", "pussy_eating",
+                              "ride", "rimjob", "rpanda", "selfsuck", "straight_bang", "suck",
+                              "tentacles", "toys", "videos", "vore", "voreforal", "vorefunbirth", "voremanal",
+                              "voremcock", "voremoral", "yiff"]
+
+            all_endpoints = sfw_endpoints + nsfw_endpoints
+
+            if endpoint is None:
+                await inter.response.defer()
+                options = [discord.SelectOption(label=e, value=e) for e in all_endpoints]
+                await inter.response.send_message("Please select an endpoint:", components=[
+                    discord.ActionRow(discord.Select(options=options, custom_id="endpoint_selection"))])
+                return
+
+            if endpoint not in all_endpoints:
+                await inter.response.send_message(
+                    "Invalid endpoint, please run `?endpoints` to see all valid endpoints")
+                return
+
+            if endpoint in nsfw_endpoints and not inter.channel.is_nsfw():
+                await inter.response.send_message("This endpoint is NSFW and can only be used in NSFW channels")
+                return
+
+            api_url = f"https://sheri.bot/api/{endpoint}"
+            headers = {
+                "Authorization": f"Token {self.api_key}",
+                "User-Agent": "MagnaBot/3.0 (Python Requests) Coded by SpiritTheWalf"
+            }
+            response = requests.get(api_url, headers=headers)
+
+            if response.status_code == 200:
+                image_url = response.json().get("url")
+                if image_url:
+                    embed = discord.Embed()
+                    embed.set_image(url=image_url)
+                    embed.set_footer(text="Powered by the Sheri API")
+                    embed.add_field(name="", value=f"[🌐 Direct URL to image]({image_url})", inline=True)
+                    await inter.response.send_message(embed=embed)
+                else:
+                    await inter.response.send_message("Error: No valid image in that endpoint, please try again later.")
+            else:
+                await inter.response.send_message(
+                    f"Error: could not fetch image from {api_url}. Status code: {response.status_code}")
+        except Exception as e:
+            await inter.response.send_message(f"Error: {e}")
+
+
+async def setup(bot):
+    await bot.add_cog(SheriCog(bot))
