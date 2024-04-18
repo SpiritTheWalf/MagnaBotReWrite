585d799da13b92afaa606573e35b239d41592f8a
SpiritTheWalf <tangentytofficial@gmail.com>
2024-04-18 13:29:30 +0100

    Stopped gifs and images embedding from firing edited message listener

--- owneronly.py
+++ owneronly.py
@@ -2,11 +2,12 @@ import discord
 import sqlite3
 import logging
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 DATABASE_FILE = "logging.db"
 COMMAND_PREFIX = "?"
 OWNER_IDS = [1174000666012823565, 1108126443638116382]
+logger = logging.getLogger(__name__)
 
 
 def is_owner(ctx):
@@ -18,6 +19,8 @@ class OwnerOnly(commands.Cog):
         self.bot = bot
         self.conn = sqlite3.connect(DATABASE_FILE)
         self.cursor = self.conn.cursor()
+        self.current_status = None
+        self.refresh_status.start()
 
     async def get_logging_channels(self, guild_id):  # Defines the function to load the logging channels
         query = "SELECT message_logs, member_logs, voice_logs, mod_logs, muterole, muterole_channel FROM guilds WHERE""guild_id = ?"
@@ -42,13 +45,13 @@ class OwnerOnly(commands.Cog):
     @commands.command(name="say", hidden=True)
     @commands.check(is_owner)
     async def say(self, ctx, *, message: str = None):
-        guild = ctx.guild.id
+        guild = ctx.guild.name
         if message is None:
             await ctx.send("Please provide a message to repeat")
             return
         await ctx.send(message)
         await ctx.message.delete()
-        print(f"Spirit said {message} in {guild}")
+        logger.info(msg=f"Spirit said {message} in {guild}")
 
     @commands.command(name="sayall", hidden=True)
     @commands.check(is_owner)
@@ -63,7 +66,7 @@ class OwnerOnly(commands.Cog):
                 if logging_channel:
                     await logging_channel.send(message)
                     await ctx.message.delete()
-                    print(f"Spirit sent {message} in all guilds")
+                    logger.info(msg=f"Spirit sent {message} in all guilds")
 
     @commands.command(name="dotstatus", hidden=True)
     @commands.check(is_owner)
@@ -81,7 +84,52 @@ class OwnerOnly(commands.Cog):
         await self.bot.change_presence(status=presence_status)
         await ctx.send(f"Bot status changed to {status.capitalize()}")
         await ctx.message.delete()
-        print(f"Spirit changed bot status to {status.capitalize()}")
+        logger.info(msg=f"Spirit changed bot status to {status.capitalize()}")
+
+    @commands.command(name="nickname", hidden=True)
+    @commands.check(is_owner)
+    async def nickname(self, ctx, *, new_nickname: str):
+        for guild in self.bot.guilds:
+            try:
+                await guild.me.edit(nick=new_nickname)
+                await ctx.send(f"Bot nickname changed to {new_nickname}.")
+            except Exception as e:
+                await ctx.send(f"Failed to change bot nickname in {guild.name}: {e}.")
+
+            await ctx.message.delete()
+            logger.info(f"Spirit changed bot nickname to {new_nickname}.")
+
+    @tasks.loop(hours=1)
+    async def refresh_status(self):
+        if self.current_status:
+            await self.bot.change_presence(activity=discord.Game(name=self.current_status))
+            logger.info(f"Bot status refreshed to {self.current_status}.")
+
+    @commands.command(name="status", hidden=True)
+    @commands.check(is_owner)
+    async def status(self, ctx, *, new_status: str):
+        self.current_status = new_status
+        await self.bot.change_presence(activity=discord.Game(name=new_status))
+        await ctx.send(f"Bot status changed to {new_status}.")
+        await ctx.message.delete()
+        logger.info(f"{ctx.author} changed bot status to Playing {new_status}.")
+
+    @commands.command(name="glist", hidden=True)
+    @commands.check(is_owner)
+    async def glist(self, ctx):
+        guilds = self.bot.guilds
+        guild = ctx.guild
+        content = ""
+        for guild in guilds:
+            line = f"{guild.name} - ID: {guild.id}\n"
+            if len(content) + len(line) > 4000:
+                await ctx.send(content)
+                content = ""
+            content += line
+        if content:
+            await ctx.send(content)
+        await ctx.message.delete()
+        logger.info(msg=f"Spirit ran command glist in {guild}")
 
 
 async def setup(bot):
